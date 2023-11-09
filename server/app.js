50709b2b2073d59c1304cdd82548e16e48d3d410
chaekeun <chaen328@khu.ac.kr>
2023-11-10 01:41:31 +0900

    1110 add: socket 연결, audio encoding

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const path = require("path");
 const fs = require("fs");
+const audioFilePath = path.join(__dirname, "../piano", "pianoC.mp3");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,28 +19,21 @@ app.use("/piano", express.static(path.join(__dirname, "../piano")));
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.on("note", (data) => {
-    //시그널링 데이터를 다른 피어로 전달
-    //signal이라는 이벤트로 송신오면 메세지가 data인수에 담긴다
-    console.log(data);
-
-    //클라이언트로 메세지 송신
-    //note이라는 이벤트로 data 송신
-    io.emit("note", data);
-  });
-
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 });
 
 app.get("/socket", (req, res) => {
-  res.sendFile(path.join(__dirname + "/socket.html"));
-});
-
-app.post("/piano", (req, res) => {
-  const audioFilePath = path.join(__dirname, "../piano", "pianoC.mp3");
-  res.sendFile(audioFilePath);
+  fs.readFile(audioFilePath, { encoding: "base64" }, (err, data) => {
+    if (err) {
+      console.error("Error reading audio file: ", err);
+      return;
+    }
+    io.emit("audio", data);
+    // res.send("Audio data sent to clients");
+    res.sendFile(path.join(__dirname + "/socket.html"));
+  });
 });
 
 server.listen(PORT, () => {
