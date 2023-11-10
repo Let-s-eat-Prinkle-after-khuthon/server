dcc644a3917f554aa991bfc762dbc96296b906d3
chaekeun <chaen328@khu.ac.kr>
2023-11-10 13:02:53 +0900

    1110 edit:res.body.note받을 수 있도록

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const http = require("http");
 const cors = require("cors");
 const socketIo = require("socket.io");
+const bodyParser = require("body-parser");
 const server = http.createServer(app);
 const io = socketIo(server);
 const path = require("path");
@@ -11,15 +12,21 @@ const fs = require("fs");
 const audioFilePath = path.join(__dirname, "../piano", "pianoC.mp3");
 const instRoutes = require("./instRoutes.js");
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(cors());
 app.use(express.static(__dirname));
 app.use("/piano", express.static(path.join(__dirname, "../piano")));
 app.use("/inst", instRoutes); //'/inst' 경로에 대한 요청은 instRoutes 파일로 라우팅
+app.io = io;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 io.on("connection", (socket) => {
   console.log("User connected");
+
+  //클라이언트에서 'note'이벤트를 받았을 때 처리하는 코드
+  socket.on("note", (data) => {
+    console.log("클라이언트로부터 note 이벤트를 받았습니다: ", data);
+  });
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
--- server/instRoutes.js
+++ server/instRoutes.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
+const path = require("path");
+
+router.use(express.static(__dirname));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/", (req, res) => {
-  res.send("라우터 제대로 되는지 테스트중");
+  res.sendFile(path.join(__dirname + "/socket.html"));
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body.note);
+  res.status(200).send(req.body.note);
 });
 
 module.exports = router;
