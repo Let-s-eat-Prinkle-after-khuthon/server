6fa9f4f4ca3abe194da007c4cb8213f304691c58
chaekeun <chaen328@khu.ac.kr>
2023-11-10 17:57:04 +0900

    1110 add:react build

--- server/app.js
+++ server/app.js
@@ -9,12 +9,10 @@ const server = http.createServer(app);
 const io = socketIo(server);
 const path = require("path");
 const fs = require("fs");
-const audioFilePath = path.join(__dirname, "../piano", "pianoC.mp3");
 const instRoutes = require("./instRoutes.js");
 
 app.use(cors());
-app.use(express.static(__dirname));
-app.use("/piano", express.static(path.join(__dirname, "../piano")));
+app.use(express.static(path.join(__dirname, "../client/chaenreact/build")));
 app.use("/inst", instRoutes); //'/inst' 경로에 대한 요청은 instRoutes 파일로 라우팅
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,17 +31,8 @@ io.on("connection", (socket) => {
   });
 });
 
-app.get("/socket", (req, res) => {
-  fs.readFile(audioFilePath, { encoding: "base64" }, (err, data) => {
-    if (err) {
-      console.error("Error reading audio file: ", err);
-      return;
-    }
-    io.emit("audio", data);
-    //이거 post요청에다가 넣어서 다시 수정할거임
-    // res.send("Audio data sent to clients");
-    res.sendFile(path.join(__dirname + "/socket.html"));
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 server.listen(PORT, () => {
--- server/instRoutes.js
+++ server/instRoutes.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const path = require("path");
 const fs = require("fs");
+
 const audioFilePath = path.join(__dirname, "../sound");
 
 router.use(express.static(__dirname));
@@ -11,10 +12,6 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use((req, res, next) => {
   req.io = req.app.get("io");
   next();
-});
-
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/totalnote.html"));
 });
 
 router.post("/piano", (req, res) => {
@@ -103,77 +100,70 @@ router.post("/piano", (req, res) => {
       res.send("Default behavior...");
       break;
   }
-  //   if (note === "C") {
-  //     fs.readFile(
-  //       path.join(audioFilePath, "pianoC.mp3"),
-  //       { encoding: "base64" },
-  //       (err, data) => {
-  //         if (err) {
-  //           console.error("Error reading audio file: ", err);
-  //           return;
-  //         }
-  //         req.io.emit("audio", data);
-  //       }
-  //     );
-  //   } else if (note === "D") {
-  //     fs.readFile(
-  //       path.join(audioFilePath, "pianoD.mp3"),
-  //       { encoding: "base64" },
-  //       (err, data) => {
-  //         if (err) {
-  //           console.error("Error reading audio file: ", err);
-  //           return;
-  //         }
-  //         req.io.emit("audio", data);
-  //       }
-  //     );
-  //   } else if (note === "E") {
-  //     fs.readFile(
-  //       path.join(audioFilePath, "pianoD.mp3"),
-  //       { encoding: "base64" },
-  //       (err, data) => {
-  //         if (err) {
-  //           console.error("Error reading audio file: ", err);
-  //           return;
-  //         }
-  //         req.io.emit("audio", data);
-  //       }
-  //     );
-  //   }
 
   res.status(200).send(req.body.note);
 });
 
 router.post("/sym", (req, res) => {
   console.log(req.body.note);
-  fs.readFile(
-    path.join(audioFilePath, "sym.mp3"),
-    { encoding: "base64" },
-    (err, data) => {
-      if (err) {
-        console.error("Error reading audio file: ", err);
-        return;
+  const note = req.body.note;
+  if (note === "O") {
+    fs.readFile(
+      path.join(audioFilePath, "sym.mp3"),
+      { encoding: "base64" },
+      (err, data) => {
+        if (err) {
+          console.error("Error reading audio file: ", err);
+          return;
+        }
+        req.io.emit("audio", data);
       }
-      const io = req.app.get("io");
-      io.emit("audio", data);
-    }
-  );
+    );
+  } else {
+    res.send("sym is not playing");
+  }
   res.status(200).send(req.body.note);
 });
 
 router.post("/tri", (req, res) => {
   console.log(req.body.note);
-  //py로부터 계이름 post요청을 받았으면 이제 홈url로 그에 맞는 audio파일을 보내주어야한다.
-  const io = req.app.get("io");
-  io.emit("note", req.body.note);
+  const note = req.body.note;
+  if (note === "O") {
+    fs.readFile(
+      path.join(audioFilePath, "sym.mp3"),
+      { encoding: "base64" },
+      (err, data) => {
+        if (err) {
+          console.error("Error reading audio file: ", err);
+          return;
+        }
+        req.io.emit("audio", data);
+      }
+    );
+  } else {
+    res.send("sym is not playing");
+  }
   res.status(200).send(req.body.note);
 });
 
 router.post("/cats", (req, res) => {
   console.log(req.body.note);
-  //py로부터 계이름 post요청을 받았으면 이제 홈url로 그에 맞는 audio파일을 보내주어야한다.
-  const io = req.app.get("io");
-  io.emit("note", req.body.note);
+  const note = req.body.note;
+  if (note === "O") {
+    fs.readFile(
+      path.join(audioFilePath, "cats.mp3"),
+      { encoding: "base64" },
+      (err, data) => {
+        if (err) {
+          console.error("Error reading audio file: ", err);
+          return;
+        }
+        req.io.emit("audio", data);
+      }
+    );
+  } else {
+    res.send("sym is not playing");
+  }
   res.status(200).send(req.body.note);
 });
 
